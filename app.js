9161d313aa90382c7c80a99123a9e0e943fe8784
nizan <nizan.shookroun@gmail.com>
2023-09-28 16:48:11 +0300

    add mongo config

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const posts = require('./routes/post');
 
-const app = express()
+require('custom-env').env(process.env.NODE_ENV, './config');
 
-app.use(express.static('public'))
+mongoose.connect(process.env.CONNECTION_STRING,
+                {   useNewUrlParser: true,
+                    useUnifiedTopology: true });
 
-app.use('/', )
+var app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
 
-app.listen(8800)
+app.use('/posts', posts);
+
+app.listen(process.env.PORT);
