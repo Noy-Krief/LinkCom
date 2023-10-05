968bd2af54087c19eae18d924a66ab806dd9b322
Noy-Krief <kriefnoy@gmail.com>
2023-10-05 15:15:25 +0300

    Fix email varification

--- controllers/login.js
+++ controllers/login.js
@@ -21,7 +21,7 @@ function login() {
     const passwordInput = document.getElementById("password");
     const loginButton = document.getElementById("loginButton");
     const errorMessage = document.getElementById("errorMessage");
-    const Account = require('../models/account')
+    // const Account = require('../models/account')
 
     const email = emailInput.value;
     const password = passwordInput.value;
@@ -41,8 +41,8 @@ function login() {
     // Replace this with your authentication logic
     if (email === "example@email.com" && password === "password123") {
         // Successful login, you can redirect or perform other actions here
-        // alert("Login successful!");
-        window.location.href = "../views/map.html"
+        alert("Login successful!");
+        //window.location.href = "../views/map.html"
         
     } else {
         // Failed login attempt, show an alert
@@ -52,23 +52,3 @@ function login() {
 
 // Add a click event listener to the button
 document.getElementById("loginButton").addEventListener("click", login);
-
-
-const mongoose = require('mongoose');
-
-// Define the schema for the "accounts" collection
-const accountSchema = new mongoose.Schema({
-  display_name: String,
-  user_name: String,
-  password: String,
-  groups: [String],
-  posts: Number,
-  events: [String],
-  future_revenges: [String],
-  profile_picture: String,
-});
-
-// Create a model for the "accounts" collection
-const Account = mongoose.model('Account', accountSchema);
-
-module.exports = Account;
